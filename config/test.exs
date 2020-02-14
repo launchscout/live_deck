e7a58579ee9ab9eea32edec39dd8605cf8659a24
Zack Kayser <zkayser@gmail.com>
2020-02-14 09:46:17 -0500

    Add real secrets

--- config/test.exs
+++ config/test.exs
@@ -15,8 +15,8 @@ config :live_deck, path_module: LiveDeck.Fakes.Path
 # you can enable the server option below.
 config :live_deck, LiveDeckWeb.Endpoint,
   http: [port: 4002],
-  secret_key_base: System.get_env("SECRET_KEY_BASE") || "thisisasupersecretsecretkeybase",
-  live_view: [signing_salt: System.get_env("LIVE_VIEW_SALT") || "thissaltisfortestingenvonly"],
+  secret_key_base: System.get_env("SECRET_KEY_BASE") || "B6l0Z1Jb1CYfqVed1oJNRlxWknzsNj4Cuqqr+iAVsPm7+ISaUzSzvOz6dSfSqmWt",
+  live_view: [signing_salt: System.get_env("LIVE_VIEW_SALT") || "B6l0Z1Jb1CYfqVed1oJNRlxWknzsNj4Cuqqr+iAVsPm7+ISaUzSzvOz6dSfSqmWt"],
   server: false
 
 # Print only warnings and errors during test
