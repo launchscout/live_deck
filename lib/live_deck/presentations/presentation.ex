6d5fc89727613f791789320951cce1d4afa0ae94
Zack Kayser <zkayser@gmail.com>
2020-02-14 15:32:39 -0500

    Add Presentation struct

---
+++ lib/live_deck/presentations/presentation.ex
@@ -0,0 +1,28 @@
+defmodule LiveDeck.Presentations.Presentation do
+  @moduledoc """
+  Exposes a Presentation struct and functions for
+  working with Presentations.
+  """
+  alias LiveDeck.Presentations.Slide
+
+  @slides Slide.all()
+  @enforce_keys [:slides, :last_slide]
+  defstruct slides: [],
+            last_slide: nil
+
+  @type t() :: %__MODULE__{
+    slides: list(String.t()),
+    last_slide: non_neg_integer()
+  }
+
+  @doc """
+  Instantiates a new Presentation.
+  """
+  @spec new() :: t()
+  def new do
+    %__MODULE__{
+      slides: @slides,
+      last_slide: length(@slides) - 1
+    }
+  end
+end
