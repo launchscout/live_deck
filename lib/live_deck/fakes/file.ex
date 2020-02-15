1843ba6dd1cb3038b84ebeb6f3221b40a910561e
Zack Kayser <zkayser@gmail.com>
2020-02-15 15:57:18 -0500

    Remove unused fakes

--- lib/live_deck/fakes/file.ex
+++
@@ -1,9 +0,0 @@
-defmodule LiveDeck.Fakes.File do
-  @moduledoc false
-
-  @files ~w(slide_1.html.eex slide_2.html.eex slide_3.html.eex slide_4.html.eex)
-
-  def ls!(_path) do
-    @files
-  end
-end
