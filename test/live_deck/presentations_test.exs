496666eb4955cba60b0104ecfbb29d9a64919437
Zack Kayser <zkayser@gmail.com>
2020-02-15 15:14:28 -0500

    Write actual files in test environment

--- test/live_deck/presentations_test.exs
+++ test/live_deck/presentations_test.exs
@@ -2,11 +2,27 @@ defmodule LiveDeck.PresentationsTest do
   alias LiveDeck.Presentations
   use ExUnit.Case
 
+  @temp_slides ~w(slide_1.html.eex slide_2.html.eex slide_3.html.eex)
+
   describe "list_slides/0" do
+    setup :create_temp_slides
+
     test "returns a list of all slides" do
       for slide <- Presentations.list_slides() do
         assert String.ends_with?(slide, ".html")
       end
     end
+  end
+
+  defp create_temp_slides(_) do
+    path = Path.expand("../lib/live_deck_web/templates/slide/", __DIR__)
+    for slide <- @temp_slides do
+      File.write("#{path}#{slide}", "<div>This is slide: #{slide}</div>")
+    end
+    on_exit(fn ->
+      for slide <- @temp_slides do
+        File.rm_rf("#{path}#{slide}")
+      end
+    end)
   end
 end
