6475bb41834c647f68493ed2683860290ae55db1
Zack Kayser <zkayser@gmail.com>
2020-02-15 15:14:28 -0500

    Write actual files in test environment

--- config/test.exs
+++ config/test.exs
@@ -8,11 +8,6 @@ config :live_deck, LiveDeck.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
-# Test setup
-config :live_deck,
-  path_module: LiveDeck.Fakes.Path,
-  file_module: LiveDeck.Fakes.File
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :live_deck, LiveDeckWeb.Endpoint,
--- lib/live_deck/presentations/slide.ex
+++ lib/live_deck/presentations/slide.ex
@@ -3,11 +3,9 @@ defmodule LiveDeck.Presentations.Slide do
   This module exposes functions for working
   with Slides.
   """
-  @path Application.get_env(:live_deck, :path_module) || Path
-  @file_mod Application.get_env(:live_deck, :file_module) || File
-  @slides_dir "../../live_deck_web/templates/slide/" |> @path.expand(__DIR__)
-  @slides (for slide <- @slides_dir |> @file_mod.ls!() do
-             @external_resource @path.relative_to_cwd(@slides_dir <> slide)
+  @slides_dir "../../live_deck_web/templates/slide/" |> Path.expand(__DIR__)
+  @slides (for slide <- @slides_dir |> File.ls!() do
+             @external_resource Path.relative_to_cwd(@slides_dir <> slide)
              String.replace(slide, ".eex", "")
            end)
 
