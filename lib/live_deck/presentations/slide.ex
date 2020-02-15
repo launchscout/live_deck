29e5a2b505c19ef6e7f8a2ddcde2c217bc5e00e9
Zack Kayser <zkayser@gmail.com>
2020-02-15 16:14:50 -0500

    Use safe version of File.ls/1

--- lib/live_deck/presentations/slide.ex
+++ lib/live_deck/presentations/slide.ex
@@ -7,8 +7,15 @@ defmodule LiveDeck.Presentations.Slide do
   @slides_dir "../../live_deck_web/templates/slide/" |> Path.expand(__DIR__)
 
   def all do
-    for slide <- File.ls!(@slides_dir) do
-      String.replace(slide, ".eex", "")
+    case File.ls(@slides_dir) do
+      {:error, _} ->
+        File.mkdir_p(@slides_dir)
+        []
+
+      {:ok, slides} ->
+        for slide <- slides do
+          String.replace(slide, ".eex", "")
+        end
     end
   end
 end
