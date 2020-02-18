95f3e5e4b945b6b47a278a8c43fe1757d40ebc50
Zack Kayser <zkayser@gmail.com>
2020-02-18 12:19:52 -0500

    Remove presentation controller

--- lib/live_deck_web/controllers/presentation_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule LiveDeckWeb.PresentationController do
-  use LiveDeckWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
--- test/live_deck_web/controllers/presentation_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule LiveDeckWeb.PresentationControllerTest do
-  use LiveDeckWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200)
-  end
-end
