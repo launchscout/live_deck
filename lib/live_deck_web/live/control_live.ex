85ad266d04d88311bffdc19f5df00e0012c76de6
Sanjay Nelson <sanjaywnelson@gmail.com>
2020-03-24 15:07:50 -0400

    writing tests for ControlLive

--- lib/live_deck_web/live/control_live.ex
+++ lib/live_deck_web/live/control_live.ex
@@ -3,9 +3,16 @@ defmodule LiveDeckWeb.ControlLive do
     Live view for control route
   """
   use Phoenix.LiveView
+  alias LiveDeck.Controls
   require Logger
 
   def mount(_params, _session, socket) do
+    Controls.start()
+    presentation = Controls.get_presentation()
+    socket =
+      socket
+      |> assign(presentation: presentation)
+      |> assign(current_slide: presentation.active_index + 1)
     {:ok, socket}
   end
 
