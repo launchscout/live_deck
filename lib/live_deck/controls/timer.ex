ac15942f4cf6b3c3f295b9e639a1ab1fb8cd231f
Zack Kayser <zkayser@gmail.com>
2020-04-16 15:27:34 -0400

    Move timer-related functionality into its own module

--- lib/live_deck/controls/timer.ex
+++ lib/live_deck/controls/timer.ex
@@ -1,9 +1,13 @@
 defmodule LiveDeck.Controls.Timer do
+  @moduledoc """
+  Exposes a Timer struct and associated functions for
+  working with timers.
+  """
   defstruct tref: nil,
             time: ~T[00:00:00.00]
 
   @type t :: %__MODULE__{
-          tref: :timer.tref(),
+          tref: :timer.tref() | nil,
           time: Time.t()
         }
 
@@ -23,5 +27,33 @@ defmodule LiveDeck.Controls.Timer do
     :timer.cancel(timer.tref)
 
     init()
+  end
+
+  @spec tick(t()) :: t()
+  def tick(timer) do
+    time = Time.add(timer.time, 1)
+    %__MODULE__{timer | time: time}
+  end
+
+  defimpl String.Chars, for: __MODULE__ do
+    def to_string(timer) do
+      to_mm_ss(timer.time)
+    end
+
+    defp to_mm_ss(time) do
+      elapsed = Time.diff(time, ~T[00:00:00.00])
+
+      mm = div(elapsed, 60)
+      ss = rem(elapsed, 60) |> format_digit
+
+      "#{mm}:#{ss}"
+    end
+
+    defp format_digit(unit) do
+      case unit < 10 do
+        true -> "0#{unit}"
+        false -> "#{unit}"
+      end
+    end
   end
 end
