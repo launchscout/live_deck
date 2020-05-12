26189b2bac40d03caa1a831eb2c3e82b4867c0c0
Ulugbek Aripov <ulugbeks@gmail.com>
2020-05-12 19:29:17 -0400

    Add ex_check to run checks with one command

--- mix.exs
+++ mix.exs
@@ -55,7 +55,10 @@ defmodule LiveDeck.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ex_check, ">= 0.0.0", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:sobelow, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
