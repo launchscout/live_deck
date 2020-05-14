c70ec321fdbe370344dc8615e7bd7d6adc3729ae
Beau <beau@gaslight.co>
2020-05-14 09:07:18 -0400

    add templates for 404 and 500 pages

--- lib/live_deck_web/views/error_view.ex
+++ lib/live_deck_web/views/error_view.ex
@@ -1,15 +1,14 @@
 defmodule LiveDeckWeb.ErrorView do
   use LiveDeckWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.html", _assigns) do
-  #   "Internal Server Error"
-  # end
+  def render("404.html", assigns) do
+    render("404_page.html", assigns)
+  end
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.html" becomes
-  # "Not Found".
+  def render("500.html", assigns) do
+    render("500_page.html", assigns)
+  end
+
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
   end
