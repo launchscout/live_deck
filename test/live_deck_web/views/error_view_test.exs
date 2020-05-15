81b3c9fc342102301ebf0ea1da6ba7d82d6d9949
Zack Kayser <zkayser@gmail.com>
2020-05-15 11:18:20 -0400

    Adjust assertions on error_view_test

--- test/live_deck_web/views/error_view_test.exs
+++ test/live_deck_web/views/error_view_test.exs
@@ -4,11 +4,26 @@ defmodule LiveDeckWeb.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test "renders 404.html" do
-    assert render_to_string(LiveDeckWeb.ErrorView, "404.html", []) == "Not Found"
+  setup context do
+    private_assigns = context.conn.private
+
+    updated =
+      Map.put(
+        context.conn,
+        :private,
+        Map.put(private_assigns, :phoenix_endpoint, LiveDeckWeb.Endpoint)
+      )
+
+    {:ok, Map.merge(context, %{conn: updated})}
   end
 
-  test "renders 500.html" do
-    assert render_to_string(LiveDeckWeb.ErrorView, "500.html", []) == "Internal Server Error"
+  test "renders 404.html", %{conn: conn} do
+    assert render_to_string(LiveDeckWeb.ErrorView, "404.html", conn: conn) =~
+             ~s(data-testid="404-page")
+  end
+
+  test "renders 500.html", %{conn: conn} do
+    assert render_to_string(LiveDeckWeb.ErrorView, "500.html", conn: conn) =~
+             ~s(data-testid="500-page")
   end
 end
