d0a1bbf825208cecc340ce71d3704369af95851f
Zack Kayser <zkayser@gmail.com>
2020-06-02 09:20:23 -0400

    Remove content-security policy preventing assets from loading

--- lib/live_deck_web/router.ex
+++ lib/live_deck_web/router.ex
@@ -7,7 +7,7 @@ defmodule LiveDeckWeb.Router do
     plug :fetch_session
     plug :fetch_live_flash
     plug :protect_from_forgery
-    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'"}
+    plug :put_secure_browser_headers
     plug :put_root_layout, {LiveDeckWeb.LayoutView, :root}
   end
 
