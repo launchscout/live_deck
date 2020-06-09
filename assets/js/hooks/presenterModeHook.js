6fac11ffb43aad93e9a483f8a128fcef69dcb414
Zack Kayser <zkayser@gmail.com>
2020-06-09 09:09:11 -0400

    Remove console.logs from PresenterModeHook

--- assets/js/hooks/presenterModeHook.js
+++ assets/js/hooks/presenterModeHook.js
@@ -1,9 +1,7 @@
 export const PresenterModeHook = {
   mounted() {
-    console.log('PresenterModeHook mounted. Pushing width initially...');
     this.pushWidth();
     window.addEventListener("resize", () => {
-      console.log('Resize event. Pushing width again...')
       this.pushWidth()
     })
   },
