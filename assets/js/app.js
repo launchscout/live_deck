5a1419ec4d57e9b34c51a8d884032bac4dd1b914
Zack Kayser <zkayser@gmail.com>
2020-06-15 08:57:15 -0400

    Remove webpack-related imports in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,3 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
-// import css from "../css/app.scss"
-// import themeCSS from "../css/app-live-deck.scss"
-
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
 import "phoenix_html"
 
 import { Socket } from "phoenix"
